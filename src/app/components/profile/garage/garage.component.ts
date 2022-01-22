200fb2135f770fa36c65a0af41fde2b5ee0ac0ec
jnlvrs <71854392+jnlvrs@users.noreply.github.com>
2022-01-21 19:19:29 -0600

    started on garage

--- src/app/components/profile/garage/garage.component.ts
+++ src/app/components/profile/garage/garage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faMotorcycle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-garage',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./garage.component.css']
 })
 export class GarageComponent implements OnInit {
-
+  faMotorcycle = faMotorcycle;
   constructor() { }
 
   ngOnInit(): void {
